dd87e75e1eafd04b9ecc65c953935b71f94fe1f5
Riyansh Biswas <charmingbiswas@gmail.com>
2023-12-09 04:41:09 +0530

    Added basic root level routing

---
+++ src/rootLayout.tsx
@@ -0,0 +1,11 @@
+import Sidebar from './components/Sidebar/Sidebar';
+
+function RootLayout() {
+    return (
+        <>
+            <Sidebar />
+        </>
+    );
+}
+
+export default RootLayout;
